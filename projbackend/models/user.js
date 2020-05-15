c845841ba130f14f3b7f5d25ac90f8d4b5ca8ce7
pandykad <47741444+pandykad@users.noreply.github.com>
2020-05-15 16:50:36 +0530

    uuid 3.4 updated, user schema completed

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,4 +1,7 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const crypto = require('crypto');
+import { v1 as uuidv1 } from 'uuid';
+
 
 
     var userSchema = new mongoose.Schema({
@@ -6,7 +9,7 @@ const mongoose = require("mongoose")
             type: String,
             required: true,
             maxlength: 32,
-            trim: true
+            trim: true,
         },
         lastname:{
             type: String,
@@ -23,9 +26,9 @@ const mongoose = require("mongoose")
             type: String,
             trim: true,
         },
-        password:{
+        encry_password:{
             type: String,
-            trim: true,
+            required: true,
         },
         salt: String,
         role:{
@@ -37,5 +40,32 @@ const mongoose = require("mongoose")
             default:[]
         }
 });
+
+userSchema.virtual("password")
+    .set(function(password){
+        this._password = password
+        this.salt = uuidv1()
+        this.encry_password = this.securePassword(password)
+    })
+    .get(function(){
+        return this._password
+    })
+
+userSchema.method = {
+
+    authenticate: function(plainpassword){
+        return this.securePassword(plainpassword) === this.encry_password
+    },
+    securePassword: function(plainpassword){
+        if(!password) return "";
+        try {
+            return crypto.createHmac('sha256', this.salt)
+                   .update(plainpassword)
+                   .digest('hex');
+        } catch (err) {
+            return "";
+        } 
+    }
+}
 
 module.exports =mongoose.model("User", userSchema)
