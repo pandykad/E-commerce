01a4fa844ce55309844332f1f35a2402fce6820e
pandykad <47741444+pandykad@users.noreply.github.com>
2020-05-31 03:25:52 +0530

    backend order remaining

--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -3,15 +3,25 @@ const router = express.Router()
 
 const {getUserById} = require("../controlllers/user")
 const {isSignedin, isAuthenticated, isAdmin} = require("../controlllers/auth")
-const {getProductById} = require("../controlllers/product")
+const {getProductById, createProduct, getProduct, photo, updateProduct, deleteProduct, getAllProduct, getAllUniqueCategories} = require("../controlllers/product")
 
 //params
 router.param("userId", getUserById)
 router.param("productId", getProductById)
 
-
-
-
-
+//actual routers
+//create
+router.post("/product/create/:userId", isSignedin, isAuthenticated, isAdmin, createProduct)
+//read
+router.get("/product/:productId", getProduct)
+router.get("/product/photo/:productId", photo)
+//update
+router.put("/product/:productId/:userId", isSignedin, isAuthenticated, isAdmin, updateProduct)
+//delete
+router.delete("/product/:productId/:userId", isSignedin, isAuthenticated, isAdmin, deleteProduct)
+//listing
+router.get("/products", getAllProduct)
+//listing categories
+router.get("/products/categories", getAllUniqueCategories)
 
 module.exports = router
